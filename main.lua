61bd26a795ff57d3e18de20b31fd2ab379e7d08a
Exceqt <godlygmgamer@gmail.com>
2024-07-02 19:22:19 -0400

    Added listening for "!banlist"

--- main.lua
+++ main.lua
@@ -1,14 +1,32 @@
 local discordia = require('discordia')
 local client = discordia.Client()
+local token = require("./token.lua")
+
+local dcmd = require("discordia-slash")
 
 client:on('ready', function()
   print('Logged in as ' .. client.user.username)
 end)
 
+local fban_list = { "12345", "90210" }
+
 client:on('messageCreate', function(message)
+  
   if message.content == '!ping' then
     message.channel:send('YOU ARE A REAL KING, GG')
+
+  elseif message.content == '!fban list' then
+
+    local total = ""
+    for _, v in pairs(fban_list) do
+      total = total .. ", " .. v
+    end
+    message.channel:send(total)
+
+  elseif message.content == '!fban add' then
+
   end
 end)
 
-client:run('Bot ')
+
+client:run('Bot ' .. token)
