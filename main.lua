d84112b457b4ffadb0cdd0f528859ebaed0a94b9
Exceqt <godlygmgamer@gmail.com>
2024-07-02 20:10:09 -0400

    Completed "!fban add" command (V1.0) Added new command "!echo" (for funsies)

--- main.lua
+++ main.lua
@@ -8,7 +8,7 @@ client:on('ready', function()
   print('Logged in as ' .. client.user.username)
 end)
 
-local fban_list = { "12345", "90210" }
+local fban_list = {}
 
 client:on('messageCreate', function(message)
   
@@ -19,12 +19,24 @@ client:on('messageCreate', function(message)
 
     local total = ""
     for _, v in pairs(fban_list) do
-      total = total .. ", " .. v
+      total = total .. " " .. v
     end
     message.channel:send(total)
 
-  elseif message.content == '!fban add' then
+  elseif message.content:sub(1, 9) == '!fban add' then
 
+    local userID = message.content:sub(11)
+
+    if #userID ~= 18 or userID == "" then
+      message.channel:send("Please enter a valid user ID (18 digits)")
+    else
+      table.insert(fban_list, userID)
+      message.channel:send("Added " .. userID .. " into fban list")
+    end
+
+  elseif message.content:sub(1, 5) == "!echo" then
+    local echoText = message.content:sub(7)
+    message.channel:send(echoText)
   end
 end)
 
